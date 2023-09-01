5a2028a50534bd7d25134ab84736888e41b2258c
Afshin Izadnia <izadnia@yahoo.com>
2023-09-01 23:48:59 +0430

    useContex seems Good

--- app/components/fixedtools/sidebar/page.tsx
+++ app/components/fixedtools/sidebar/page.tsx
@@ -1,7 +1,6 @@
-"use client";
 
 import UserProfile from "./UserProfile";
-
+import { SidebarContext } from "../page";
 import Link from "next/link";
 import { useSelectedLayoutSegment } from "next/navigation";
 
@@ -13,7 +12,7 @@ import {
   UsersIcon,
   VideosIcon,
 } from "./icons";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 const menuItems = [
   { id: 1, label: "Home", icon: HomeIcon, link: "/" },
@@ -24,51 +23,49 @@ const menuItems = [
 
 function SideBar({ sideOpen }: any) {
   const segment = useSelectedLayoutSegment();
-  const [sideBar, setSideBar] = useState(Boolean);
-  useEffect(() => {
-    if (sideOpen) {
-      setSideBar(true);
-    } else {
-      setSideBar(false);
-    }
-    console.log(sideOpen);
-  }, [sideOpen]);
+  const { isSidebarOpen, setIsSidebarOpen } = useContext(SidebarContext);
+  console.log(isSidebarOpen);
+  function handleItemClick() {
+    setIsSidebarOpen(false);
+  }
 
   return (
     <div
       className={`bg-slate-400  min-h-screen ${
-        sideBar ? "w-[320px] duration-300 p-4" : "w-0 duration-700"
+        isSidebarOpen ? "w-[320px] duration-300 p-4" : "w-0 duration-700"
       }`}
     >
-      <div className={sideBar ? "visible" : "hidden"}>
+      <div className={isSidebarOpen ? "visible" : "hidden"}>
         <UserProfile login={true} username={"user"} />
 
-        {menuItems.map(({ icon: Icon, ...menu }) => {
-          return (
-            <Link key={menu.id + menu.label} href={menu.link} onClick={()=>{setSideBar(false)}}>
-              <div
-                className={`flex rounded-xl p-3 m-1 ${
-                  segment == menu.label ? "bg-slate-600 text-white" : null
-                } ${
-                  segment == null && menu.label == "Home"
-                    ? "bg-slate-600 text-white"
-                    : null
-                } hover:text-white hover:bg-slate-500`}
-              >
-                <div className="flex">
-                  <div style={{ width: "2.5rem" }}>
-                    <Icon />
-                  </div>
-                  {
-                    <span className="text-md font-medium text-text-light">
-                      {menu.label}
-                    </span>
-                  }
+        {menuItems.map(({ icon: Icon, ...menu }) => (
+          <Link
+            key={menu.id + menu.label}
+            href={menu.link}
+            onClick={handleItemClick}
+          >
+            <div
+              className={`flex rounded-xl p-3 m-1 ${
+                segment == menu.label ? "bg-slate-600 text-white" : null
+              } ${
+                segment == null && menu.label == "Home"
+                  ? "bg-slate-600 text-white"
+                  : null
+              } hover:text-white hover:bg-slate-500`}
+            >
+              <div className="flex">
+                <div style={{ width: "2.5rem" }}>
+                  <Icon />
                 </div>
+                {
+                  <span className="text-md font-medium text-text-light">
+                    {menu.label}
+                  </span>
+                }
               </div>
-            </Link>
-          );
-        })}
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
