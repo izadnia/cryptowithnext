17e7ae6340c0a48230660f722661bfc2a665e63b
Afshin Izadnia <izadnia@yahoo.com>
2023-09-09 00:13:22 +0430

    slider updated

--- app/components/Slider/Slider.tsx
+++ app/components/Slider/Slider.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "../../styles/Style.module.scss";
+import gsap from "gsap";
+import { setTimeout } from "timers";
+import { AccessibilityModifier } from "typescript";
+
 function Slider() {
   const sliderdata = [
     {
@@ -15,30 +20,150 @@ function Slider() {
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
     },
     {
-      title:"I'm the forth box",
-    description:"Lorem ipsum dolor sit amet, consectetur adipiscing"
+      title: "I'm the forth box",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
     },
     {
-      title:"I'm the fifth box",
-    description:"Lorem ipsum dolor sit amet, consectetur adipiscing"
-    }
+      title: "I'm the fifth box",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+    },
   ];
 
-  
+  const [value, setValue] = useState(0);
+  const [trailValue, setTrailValue] = useState(0);
+  const [trigger, setTrigger] = useState(true);
+  let interval = 4000;
+
+  function trailupdat(value: number, trailValue: number) {
+    if (value === 0) {
+      trailValue = 0;
+    } else if (value === 20) {
+      trailValue = 1;
+    } else if (value === 40) {
+      trailValue = 2;
+    } else if (value === 60) {
+      trailValue = 3;
+    } else {
+      trailValue = 4;
+    }
+  }
+
+  function trailupdate(value: number) {
+    if (value === 0) {
+      setTrailValue(0);
+    } else if (value === 20) {
+      setTrailValue(1);
+    } else if (value === 40) {
+      setTrailValue(2);
+    } else if (value === 60) {
+      setTrailValue(3);
+    } else {
+      setTrailValue(4);
+    }
+  }
+
+  function slide(condition: string, number: number) {
+    if (trigger) {
+      setTimeout(() => {
+        initiateINC(number);
+        const tl = gsap.timeline({
+          defaults: { duration: 1, ease: "power2.inOut" },
+        });
+        tl.from(".bg-slider", { x: "-100%", opacity: 0 })
+          .from(".p-slider", { opacity: 0 }, "-=0.3")
+          .from(".h-slider", { opacity: 0, y: "30px" }, "-=0.3");
+      }, interval);
+    }
+    setTrigger(false);
+  }
+
+  function initiateINC(num: number) {
+    value === 80 ? setValue(0) : setValue(num + 20);
+    trailupdate(value);
+  }
+
+  useEffect(() => {
+    slide("increase", value);
+  }, [value]);
+
   return (
     <div className={styles.parent}>
       <div className={styles.container}>
-        <div className={styles.slider}>
+        <div
+          id="slider"
+          style={{ translate: `-${value}%` }}
+          className={styles.slider}
+        >
           <div className={`${styles.box} ${styles.box1}`}>
-            <div className={styles.bg}></div>
+            <div className={`${styles.bg} bg-slider`}></div>
             <div className={styles.details}>
-              <h1>I'm the first Box</h1>
-              <p>
+              <h1 className="h-slider">I'm the 1 Box</h1>
+              <p className="p-slider">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
                 lacinia dui lectus. Donec scelerisque ipsum diam, ac mattis orci
                 pellentesque eget.
               </p>
-              <button>Check Now</button>
+              <button className="button-slider">Check Now1</button>
+            </div>
+            <div className={styles.illustration}>
+              <div className={styles.inner}></div>
+            </div>
+          </div>
+          <div className={`${styles.box} ${styles.box2}`}>
+            <div className={`${styles.bg} bg-slider`}></div>
+            <div className={styles.details}>
+              <h1 className="h-slider">I'm the 2 Box</h1>
+              <p className="p-slider">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                lacinia dui lectus. Donec scelerisque ipsum diam, ac mattis orci
+                pellentesque eget.
+              </p>
+              <button className="button-slider">Check Now2</button>
+            </div>
+            <div className={styles.illustration}>
+              <div className={styles.inner}></div>
+            </div>
+          </div>
+          <div className={`${styles.box} ${styles.box3}`}>
+            <div className={`${styles.bg} bg-slider`}></div>
+            <div className={styles.details}>
+              <h1 className="h-slider">I'm the 3 Box</h1>
+              <p className="p-slider">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                lacinia dui lectus. Donec scelerisque ipsum diam, ac mattis orci
+                pellentesque eget.
+              </p>
+              <button className="button-slider">Check Now3</button>
+            </div>
+            <div className={styles.illustration}>
+              <div className={styles.inner}></div>
+            </div>
+          </div>
+          <div className={`${styles.box} ${styles.box4}`}>
+            <div className={`${styles.bg} bg-slider`}></div>
+            <div className={styles.details}>
+              <h1 className="h-slider">I'm the 4 Box</h1>
+              <p className="p-slider">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                lacinia dui lectus. Donec scelerisque ipsum diam, ac mattis orci
+                pellentesque eget.
+              </p>
+              <button className="button-slider">Check Now4</button>
+            </div>
+            <div className={styles.illustration}>
+              <div className={styles.inner}></div>
+            </div>
+          </div>
+          <div className={`${styles.box} ${styles.box5}`}>
+            <div className={`${styles.bg} bg-slider`}></div>
+            <div className={styles.details}>
+              <h1 className="h-slider">I'm the 5 Box</h1>
+              <p className="p-slider">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                lacinia dui lectus. Donec scelerisque ipsum diam, ac mattis orci
+                pellentesque eget.
+              </p>
+              <button className="button-slider">Check Now5</button>
             </div>
             <div className={styles.illustration}>
               <div className={styles.inner}></div>
@@ -46,38 +171,37 @@ function Slider() {
           </div>
         </div>
 
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className={styles.prev}
-          width="36.898"
-          height="41"
-          viewBox="0 0 56.898 91"
-        >
-          <path
-            d="M45.5,0,91,56.9,48.452,24.068,0,56.9Z"
-            transform="translate(0 91) rotate(-90)"
-            fill="#fff"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className={styles.next}
-          width="36.898"
-          height="41"
-          viewBox="0 0 56.898 91"
-        >
-          <path
-            d="M45.5,0,91,56.9,48.452,24.068,0,56.9Z"
-            transform="translate(56.898) rotate(90)"
-            fill="#fff"
-          />
-        </svg>
-        <div className={styles.trail}>
-          <div className={`${styles.box1} ${styles.active}`}></div>
-          <div className={styles.box2}></div>
-          <div className={styles.box3}></div>
-          <div className={styles.box4}></div>
-          <div className={styles.box5}></div>
+        <div id="trail" className={styles.trail}>
+          <div
+            onClick={() => {
+              setTrigger(false), setValue(0), setTrigger(true);
+            }}
+            className={`${styles.box1} ${value === 0 ? styles.active : null}`}
+          ></div>
+          <div
+            onClick={() => {
+              setTrigger(false), setValue(20), setTrigger(true);
+            }}
+            className={`${styles.box2} ${value === 20 ? styles.active : null}`}
+          ></div>
+          <div
+            onClick={() => {
+              setTrigger(false), setValue(40), setTrigger(true);
+            }}
+            className={`${styles.box3} ${value === 40 ? styles.active : null}`}
+          ></div>
+          <div
+            onClick={() => {
+              setTrigger(false), setValue(60), setTrigger(true);
+            }}
+            className={`${styles.box4} ${value === 60 ? styles.active : null}`}
+          ></div>
+          <div
+            onClick={() => {
+              setTrigger(false), setValue(80), setTrigger(true);
+            }}
+            className={`${styles.box5} ${value === 80 ? styles.active : null}`}
+          ></div>
         </div>
       </div>
     </div>
