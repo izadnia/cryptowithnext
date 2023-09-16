9474e51cecc48bf4a2a97088da65e62b0b114733
Afshin Izadnia <izadnia@yahoo.com>
2023-09-16 18:59:15 +0430

    ContainerComponent added to the project

---
+++ app/components/container/ContainerComponents.tsx
@@ -0,0 +1,16 @@
+export default function ContainerComponents({
+  title,
+  titleSize,
+  children,
+}: {
+  title: string;
+  titleSize: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="w-[100%] mt-8">
+      <h1 className={` ${titleSize} my-4 px-1`}>{title}</h1>
+      {children}
+    </div>
+  );
+}
